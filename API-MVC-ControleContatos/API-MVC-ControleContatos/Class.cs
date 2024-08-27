16e107ef1c2b80668e8f885170d85b972d5d69fe
Everton Cabral <96452540+EvertoonCabral@users.noreply.github.com>
2024-08-26 23:15:57 -0300

    Criando projeto

---
+++ API-MVC-ControleContatos/API-MVC-ControleContatos/Class.cs
@@ -0,0 +1,6 @@
+﻿namespace API_MVC_ControleContatos
+{
+    public class Class
+    {
+    }
+}
---
+++ API-MVC-ControleContatos/API-MVC-ControleContatos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace API_MVC_ControleContatos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
